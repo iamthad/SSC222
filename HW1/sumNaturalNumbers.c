e94f188171ef89541f7b487ddd3079005e9d8147
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2012-09-18 16:06:02 -0500

    Wrote Fortran portion of HW1 and renamed output files from C portion to reflect language.

--- HW1/sumNaturalNumbers.c
+++ HW1/sumNaturalNumbers.c
@@ -4,7 +4,7 @@ int main(){
   const int N = 10;
   int sum;
   sum = N*(N+1)/2;
-  printf("%d is the sum of the first %d natural numbers.\n",sum,N);
+  printf("The sum of the first %d natural numbers is %d.\n",N,sum);
   return 0;
 }
 
