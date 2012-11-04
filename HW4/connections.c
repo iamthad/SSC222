4a0cd9de93700ce455343d297ec5de114d515321
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2012-11-04 13:41:37 -0600

    More progress on connections

--- HW4/connections.c
+++ HW4/connections.c
@@ -2,64 +2,97 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int count_lines(FILE *file){
-  char c = 0;
-  int num_lines = 0;
+void check_file_shape(FILE *file,char delimiter,int *lines, int *cols, int *max_fieldlength){
+  int c = 0;
+  int fieldlength = 0;
+  int tabs;
+  *lines = 0;
+  *cols = 0;
+  *max_fieldlength = 0;
   while (c != EOF){
     c = fgetc(file);
-    if (c=='\n')
-      num_lines++;
-  }
-  return(num_lines);
-}
+    if (c=='\n'){
+      (*lines)++;
+      if (fieldlength> *max_fieldlength)
+        *max_fieldlength = fieldlength;
+      fieldlength= 0;
+      if ((tabs+1)>*cols)
+        *cols = tabs+1;
+      tabs = 0;
+    }
+    if (c==delimiter){
+      tabs++;
+      if (fieldlength > *max_fieldlength)
+        *max_fieldlength = fieldlength;
+      fieldlength= 0;
+    }
+    fieldlength++;
 
+  }
+}
 
 int main(int argc, char *argv[]){
   FILE *file;
   int num_lines = 0;
-  int i,j,num_cities,matches;
-  char c = 0;
+  int num_cols = 0;
+  int max_fieldlength = 0;
+  int i,j,k,i_row,i_col,j_row,j_col,num_cities,matches;
+  int c = 0;
+  char delimiter = '\t';
   file = fopen(argv[1], "r");
   if (file == NULL) {
     fprintf(stderr, "Invalid file name.\n");
     exit(1);
   }
   // Figure out how many lines there are in the file  
-  num_lines = count_lines(file);
-
-  // There are at most 2*num_lines unique city names in the file
-  char all_names [2*num_lines][32];
-  for (i=0;i<2*num_lines;i++){
-    for (j=0;j<=32;j++){
-      all_names[i][j] = 0;
+  check_file_shape(file,delimiter,&num_lines,&num_cols,&max_fieldlength);
+  
+  // There are at most num_cols*num_lines unique fields
+  char raw_data [num_lines][num_cols][max_fieldlength];
+  for (i=0;i<num_lines;i++){
+    for (j=0;j<num_cols;j++){
+      for (k=0;k<max_fieldlength;k++){
+        raw_data[i][j][k] = 0;
       }
+    }
   }
+
 
   // Read the city names, including spaces.
   rewind(file);
   i=0;
   j=0;
-  c=0;
+  k=0;
+  c = fgetc(file);
   while(c != EOF){
-    c = fgetc(file);
-    if (c=='\n' || c=='\t'){
+    if (c=='\n'){
       i++;
       j=0;
+      k=0;
+    }
+    else if(c==delimiter){
+      j++;
+      k=0;
     }
     else{
-      all_names[i][j]=c;
-      j++;
+      raw_data[i][j][k]=c;
+      k++;
     }
+    c = fgetc(file);
   }
   
   // Count how many unique city names there are.
   num_cities=0;
-  for(i=1;i<2*num_lines;i++){
+  for(i=0;i<(num_lines*num_cols);i++){
     matches = 0;
     for(j=0;j<i;j++){
-      printf("%s : %s\n",all_names[i],all_names[j]);
+      i_row = i/(num_cols);
+      i_col = i%(num_cols);
+      j_row = j/(num_cols);
+      j_col = j%(num_cols);
+      printf("[%d,%d] %s : [%d,%d] %s\n",i_row,i_col,raw_data[i_row][i_col],j_row,j_col,raw_data[j_row][j_col]);
       // strcmp returns 0 if matching, non-0 if non-matching. This is non-intuitive.
-      if(!strcmp(all_names[i],all_names[j])){
+      if(!strcmp(raw_data[i_row][i_col],raw_data[j_row][j_col])){
         matches = 1;
         printf("Matches!\n");
         break;
@@ -70,7 +103,7 @@ int main(int argc, char *argv[]){
       printf("Unique!\n");
     }
   }
-  printf("%d cities, %d unique.\n",2*num_lines,num_cities);
+  printf("%d cities, %d unique.\n",num_lines*num_cols,num_cities);
 
   return 0;
 
--- HW4/gradebook_2.c
+++ HW4/gradebook_2.c
@@ -5,47 +5,42 @@
  * highest and lowest grades. */
 
 #include <stdio.h>
+#include <stdlib.h>
 #define NAME_LENGTH 32
-int main(){
+int main(int argc, char *argv[]){
   const int num_students = 10;
   int i, j, grade_temp, max_grade, max_grade_index, min_grade, min_grade_index, state;
   int char_temp;
+  FILE *file;
   typedef struct student{
-    int grade;
+    int studentID;
+    double grade;
     char name[NAME_LENGTH];
   }student;
   student students[num_students];
   min_grade = 100;
   max_grade = -1;
+
+  file = fopen(argv[1], "r");
+  if (file == NULL) {
+    fprintf(stderr, "Invalid file name.\n");
+    exit(1);
+  }
+  
   for (i=0; i < num_students; i++){
+    fscanf(file,"%d %lf", &students[i].studentID, &students[i].grade);
     for (j=0; j < NAME_LENGTH; j++){
         students[i].name[j] = 0; //Zero out each name to prevent junk characters.
         }
-    printf("What is the name of student %d?\n",i+1);
     char_temp = 0;
     for (j=0; j < NAME_LENGTH; j++){
-      char_temp = getchar();
+      char_temp = getc(file);
       if (char_temp == ('\n') || char_temp == EOF){
         break;
       }else{
         students[i].name[j] = char_temp;
       }
     }
-    state = 1;
-    // Error handling loop
-    while (state > 0){
-      printf("What is their grade (between 0 and 100, integer)?\n");
-      scanf("%d",&grade_temp);
-      //scanf leaves the \n in stdin. This prevents more names from being read
-      //So, we will manually grab that character off the buffer.
-      getchar();
-      if (grade_temp <= 100 && grade_temp >= 0){
-        state = 0;
-        students[i].grade = grade_temp;
-      } else {
-        printf("Invalid grade.\n");
-      }
-    } 
     if (students[i].grade > max_grade){
       max_grade = students[i].grade;
       max_grade_index = i;
@@ -56,7 +51,7 @@ int main(){
     }
   }
   for (i=0; i < num_students; i++){
-    printf("%s: %d", students[i].name,students[i].grade);
+    printf("%d %s: %lf", students[i].studentID,students[i].name,students[i].grade);
     if (i == max_grade_index){
       printf(" MAXIMUM");
     }
