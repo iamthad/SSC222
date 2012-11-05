89c9a7de8665a0d309fba059b3301498fe9e41b0
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2012-11-05 14:37:19 -0600

    Unique cities and connections working in connections.c

--- HW4/connections.c
+++ HW4/connections.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[]){
   int num_lines = 0;
   int num_cols = 0;
   int max_fieldlength = 0;
-  int i,j,k,i_row,i_col,j_row,j_col,num_cities,matches;
+  int i,j,k,i_row,i_col,j_row,j_col,num_cities,matches,from_index,to_index;
   int c = 0;
   char delimiter = '\t';
   file = fopen(argv[1], "r");
@@ -85,9 +85,9 @@ int main(int argc, char *argv[]){
   num_cities=0;
   for(i=0;i<(num_lines*num_cols);i++){
     matches = 0;
+    i_row = i/(num_cols);
+    i_col = i%(num_cols);
     for(j=0;j<i;j++){
-      i_row = i/(num_cols);
-      i_col = i%(num_cols);
       j_row = j/(num_cols);
       j_col = j%(num_cols);
       printf("[%d,%d] %s : [%d,%d] %s\n",i_row,i_col,raw_data[i_row][i_col],j_row,j_col,raw_data[j_row][j_col]);
@@ -104,6 +104,70 @@ int main(int argc, char *argv[]){
     }
   }
   printf("%d cities, %d unique.\n",num_lines*num_cols,num_cities);
+  char cities[num_cities][max_fieldlength];
+
+  k = 0;
+  for(i=0;i<(num_lines*num_cols);i++){
+    matches = 0;
+    i_row = i/(num_cols);
+    i_col = i%(num_cols);
+    for(j=0;j<i;j++){
+      j_row = j/(num_cols);
+      j_col = j%(num_cols);
+      // strcmp returns 0 if matching, non-0 if non-matching. This is non-intuitive.
+      if(!strcmp(raw_data[i_row][i_col],raw_data[j_row][j_col])){
+        matches = 1;
+        break;
+      }
+    }
+    if (matches==0){
+      strcpy(cities[k],raw_data[i_row][i_col]);
+      k++;
+    }
+  }
+  // Allocate connection matrix
+  int connections[num_cities][num_cities];
+
+  // -1 indicates cities are not connected
+  for(i=0;i<num_cities;i++){
+    for(j=0;j<num_cities;j++){
+      connections[i][j] = -1;
+    }
+  }
+
+  // 0 indicates same city
+  for(i=0;i<num_cities;i++){
+    connections[i][i] = 0;
+  }
+
+  // 1 indicates cities are connected. This could be replaced with a distance value if minimum-distance route was desired.
+  for(i=0;i<num_lines;i++){
+    // Find the index of the city name in cities
+    for(j=0;j<num_cities;j++){
+      if(!strcmp(raw_data[i][0],cities[j])){
+        from_index = j;
+        break;
+      }
+    }
+    for(j=0;j<num_cities;j++){
+      if(!strcmp(raw_data[i][1],cities[j])){
+        to_index = j;
+        break;
+      }
+    }
+    connections[from_index][to_index]=1;
+    /* If connections are reciprocal:
+     * connections[to_index][from_index]=1; */
+    printf("%s connects to %s\n",cities[from_index],cities[to_index]);
+  }
+  for(i=0;i<num_cities;i++){
+    for(j=0;j<num_cities;j++){
+      printf("%+d ",connections[i][j]);
+    }
+    printf("\n");
+  }
+
+
 
   return 0;
 
