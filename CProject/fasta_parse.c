407babcb32fda05658e9ff921b2d3cfc8441c41b
Thadeus Fleming <thadeus.j.fleming@gmail.com>
2012-11-20 22:27:45 -0600

    Reading through file is successful.

--- CProject/fasta_parse.c
+++ CProject/fasta_parse.c
@@ -15,6 +15,8 @@ int main(int argc, char*argv[]){
   int mode = 0;
   int location = 0;
   int sequence_length = 0;
+  int sequence_index = 0;
+  int header_index = 0;
   char defaultname[] = "input.fasta";
   if (argc < 2)
     file = fopen(defaultname, "r");
@@ -30,26 +32,37 @@ int main(int argc, char*argv[]){
     c = fgetc(file);
     switch (c){
       case '\n':
-        if (mode == SCANNING_HEADER)
+        if (mode == SCANNING_HEADER){
           mode = SCANNING_SEQUENCE;
-        if (mode == READING_HEADER)
+          printf("Scanning sequence.\n");
+        }
+
+        if (mode == READING_HEADER){
           mode = READING_SEQUENCE;
+          sequence_index = 0;
+          printf("Reading sequence.\n");
+        }
         break;
 
       case '>':
         switch (mode){
           case SCANNING_SEQUENCE:
-            fseek(file,location);
-            if (temp ~= NULL)
+            fseek(file,location,SEEK_SET);
+            if (temp != NULL)
               insertEnd(&sequences,temp);
             temp = malloc(sizeof(dln)+sequence_length*sizeof(char));
             temp -> sequence_length = sequence_length;
             sequence_length = 0;
             mode = READING_HEADER;
+            header_index = 0;
+            printf("Reading header.\n");
             break;
           case READING_SEQUENCE:
           default:
             mode = SCANNING_HEADER;
+            printf("Scanning header.\n");
+            location = ftell(file);
+            printf("Location: %d\n",location);
             break;
         }
         num_sequences++;
@@ -63,28 +76,26 @@ int main(int argc, char*argv[]){
             sequence_length++;
             break;
           case READING_HEADER:
-            if header_index > HEADER_LENGTH{
+            if (header_index > HEADER_LENGTH){
               fprintf(stderr,"Header would have overflowed.\n");
               exit(1);
             }
             temp -> header[header_index] = c;
             header_index++;
             break;
-          case READING_SEQUECE:
-            if sequence_index > temp -> sequence_length{
+          case READING_SEQUENCE:
+            if (sequence_index > temp -> sequence_length){
               fprintf(stderr,"Sequence would have overflowed.\n");
               exit(1);
             }
-            temp -> data[data_index] = c;
-            data_index++;
+            temp -> data[sequence_index] = c;
+            sequence_index++;
             break;
         }
     }
-
-
-
+  }while(c!=EOF);
+  printf("%d\n",num_sequences);
   fclose(file);
   return 0;
-  }while(c!=EOF);
 }
 
